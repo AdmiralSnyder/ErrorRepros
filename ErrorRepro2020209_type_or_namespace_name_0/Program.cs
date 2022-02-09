803e58cda4b26620d13c76614a2c898bf973be6e
Alexander Gayko <a.gayko@googlemail.com>
2022-02-09 13:27:50 +0100

    added comments

--- ErrorRepro2020209_type_or_namespace_name_0/Program.cs
+++ ErrorRepro2020209_type_or_namespace_name_0/Program.cs
@@ -6,6 +6,8 @@
     {
         private void MethodATest()
         {
+            // this yields the error message: Error CS0246  The type or namespace name '{0}' could not be found(are you missing a using directive or an assembly reference?)
+            // the MethodA method in TheClass in the class library project doesn't compile right now, because i added a comma at the end of the parameter list.
             TheClass.MethodA(1);
         }
     }
--- ErrorRepro20220209_Lib/TheClass.cs
+++ ErrorRepro20220209_Lib/TheClass.cs
@@ -2,6 +2,7 @@
 {
     public class TheClass
     {
+        // i added a comma to the parameter list - the error on the other side is misleading.
         public static bool MethodA(int x,) => true;
     }
 }
